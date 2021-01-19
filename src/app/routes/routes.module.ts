0798f51ca23c5be2941bb6f2c95c08325eee01a9
mars <zheng.xiangfei@rococo.co.jp>
2021-01-19 09:29:12 +0800

    version update

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -2,8 +2,6 @@ import { RoutesRoutingModule } from './routes-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
