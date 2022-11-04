60641fa49756474438a1295457b8c843eb3df55c
Mars Zheng <zheng.xiangfei@rococo.co.jp>
2022-11-04 11:05:12 +0800

    统一设置web title

--- src/app/core/service/startup.service.ts
+++ src/app/core/service/startup.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { ALAIN_I18N_TOKEN, Menu, MenuService } from '@delon/theme';
+import { ALAIN_I18N_TOKEN, Menu, MenuService, TitleService } from '@delon/theme';
 import { Observable, of, zip } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -12,10 +12,17 @@ import { MyApplicationService } from './my-application.service';
  */
 @Injectable()
 export class StartupService {
-  constructor(private menuService: MenuService, private myApp: MyApplicationService, @Inject(ALAIN_I18N_TOKEN) private i18n: I18NService) {}
+  constructor(
+    private menuService: MenuService,
+    private myApp: MyApplicationService,
+    @Inject(ALAIN_I18N_TOKEN) private i18n: I18NService,
+    private titleService: TitleService
+  ) {}
 
   load(): Observable<void> {
     const defaultLang = this.i18n.defaultLang;
+    this.titleService.default = this.i18n.fanyi(this.myApp.appName);
+
     return zip(this.i18n.loadLangData(defaultLang), this.myApp.getACLInfo(), this.myApp.getMenuData()).pipe(
       map(([langData, aclInfo, menu]: any[]) => {
         if (menu) {
